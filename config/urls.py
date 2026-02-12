6618932fc1d052d24eea7ae5384e770b1b20827a
AlexandraSelby <dagnaa19@gmail.com>
2026-02-12 18:46:42 +0000

    Add URL routes for main navigation pages

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from home.views import home
+from catalog.views import catalog_home
+from club.views import club_home
+from samples.views import packs_home
+from polls.views import poll_home
+from checkout.views import membership_home
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+
+    path("", home, name="home"),
+    path("catalog/", catalog_home, name="catalog"),
+    path("club/", club_home, name="club"),
+    path("packs/", packs_home, name="packs"),
+    path("poll/", poll_home, name="poll"),
+    path("membership/", membership_home, name="membership"),
 ]
