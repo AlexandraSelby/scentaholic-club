8bef99281ec683ff7a94ea2a43a5a52f69811d82
AlexandraSelby <dagnaa19@gmail.com>
2026-03-06 12:58:15 +0000

    Register catalog models in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Brand, Fragrance
 
-# Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+    list_display = ["name", "country"]
+
+
+@admin.register(Fragrance)
+class FragranceAdmin(admin.ModelAdmin):
+    search_fields = ["name", "brand__name"]
+    list_filter = ["brand", "is_active"]
+    list_display = ["name", "brand", "year_released", "is_active"]
