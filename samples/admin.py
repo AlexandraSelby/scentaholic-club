bc0d4a81608db5782cee13ef2dead04e78d99514
AlexandraSelby <dagnaa19@gmail.com>
2026-03-11 15:00:10 +0000

    Register weekly pack models in admin

--- samples/admin.py
+++ samples/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import WeeklyPack, PackItem
 
-# Register your models here.
+
+class PackItemInline(admin.TabularInline):
+    model = PackItem
+    extra = 1
+
+
+@admin.register(WeeklyPack)
+class WeeklyPackAdmin(admin.ModelAdmin):
+    list_display = ["title", "week_start", "week_end", "is_published"]
+    list_filter = ["is_published"]
+    inlines = [PackItemInline]
+
+
+@admin.register(PackItem)
+class PackItemAdmin(admin.ModelAdmin):
+    list_display = ["pack", "fragrance", "sample_size_ml"]
+    list_filter = ["pack"]
