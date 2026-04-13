a253e84a25653c0b4eeb556de8542cb3ba2c0a6d
AlexandraSelby <dagnaa19@gmail.com>
2026-04-13 12:07:10 +0100

    Add poll voting form and save submitted votes

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,40 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 
-from django.shortcuts import render
-from .models import WeeklyPoll
+from .forms import VoteForm
+from .models import Vote, WeeklyPoll
 
 
+@login_required
 def poll_home(request):
-    active_poll = WeeklyPoll.objects.filter(is_active=True).order_by("-created_at").first()
+    poll = WeeklyPoll.objects.filter(is_active=True).order_by("-created_at").first()
+
+    if not poll:
+        return render(request, "polls/poll_home.html", {"poll": None})
+
+    if request.method == "POST":
+        form = VoteForm(request.POST, poll=poll)
+        if form.is_valid():
+            selected_fragrances = form.cleaned_data["fragrances"]
+
+            Vote.objects.filter(user=request.user, poll=poll).delete()
+
+            for fragrance in selected_fragrances:
+                Vote.objects.create(
+                    user=request.user,
+                    poll=poll,
+                    fragrance=fragrance,
+                )
+
+            return redirect("poll")
+    else:
+        form = VoteForm(poll=poll)
 
     return render(
         request,
         "polls/poll_home.html",
-        {"poll": active_poll},
+        {
+            "poll": poll,
+            "form": form,
+        },
     )
