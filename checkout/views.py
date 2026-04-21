fdbed52d14143d36f1fa73b631b41e11c1e19262
AlexandraSelby <dagnaa19@gmail.com>
2026-04-21 13:54:46 +0100

    Add Stripe weekly subscription checkout views

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect, render
+import stripe
 
-from django.shortcuts import render
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
 
 def membership_home(request):
     return render(request, "checkout/membership_home.html")
 
+
+def create_checkout_session(request):
+    session = stripe.checkout.Session.create(
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price_data": {
+                    "currency": "gbp",
+                    "product_data": {
+                        "name": "Scentaholic Weekly Sample Pack Membership",
+                    },
+                    "unit_amount": 500,
+                    "recurring": {
+                        "interval": "week",
+                    },
+                },
+                "quantity": 1,
+            }
+        ],
+        mode="subscription",
+        success_url="http://127.0.0.1:8000/checkout/success/",
+        cancel_url="http://127.0.0.1:8000/checkout/cancel/",
+    )
+    return redirect(session.url)
+
+
+def success(request):
+    return render(request, "checkout/success.html")
+
+
+def cancel(request):
+    return render(request, "checkout/cancel.html")
