6e7bbff83d6f2bd585783a6d70ce195d8729e394
AlexandraSelby <dagnaa19@gmail.com>
2026-04-23 10:44:03 +0100

    Restrict signup page to anonymous users only

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import user_passes_test
 from .forms import SignUpForm
 
-
+@user_passes_test(lambda user: not user.is_authenticated, login_url="/")
 def signup(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
