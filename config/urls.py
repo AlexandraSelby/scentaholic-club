abc848c2e332b2b100a1fc8e4246c7910e229c19
AlexandraSelby <dagnaa19@gmail.com>
2026-04-23 14:27:05 +0100

    Fix URL configuration for auth, polls, and checkout

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
 
 from home.views import home
 from catalog.views import catalog_home
 from club.views import club_home
 from samples.views import packs_home
-from polls.views import poll_home
 from profiles.views import signup
 
 urlpatterns = [
-    
     path("admin/", admin.site.urls),
 
     # Auth
+    path(
+        "accounts/login/",
+        LoginView.as_view(
+            template_name="registration/login.html",
+            redirect_authenticated_user=True,
+        ),
+        name="login",
+    ),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/signup/", signup, name="signup"),
 
     # Site pages
     path("", home, name="home"),
@@ -21,7 +29,5 @@ urlpatterns = [
     path("club/", club_home, name="club"),
     path("packs/", packs_home, name="packs"),
     path("poll/", include("polls.urls")),
-    path("accounts/signup/", signup, name="signup"),
     path("membership/", include("checkout.urls")),
-    
 ]
