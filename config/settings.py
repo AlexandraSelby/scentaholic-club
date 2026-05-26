0365e103461fc9532693b9c71c2e015942a02050
AlexandraSelby <dagnaa19@gmail.com>
2026-05-26 10:28:39 +0100

    Move secret key and debug settings to environment variables

--- config/settings.py
+++ config/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vikmjs+08ng2$but*7^v-ffyoio&8+v-fbh!!$zb+fd%s!)$kp'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 # Application definition
 
